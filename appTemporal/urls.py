651a1cb1c271e1ac156dce3e9501bd43901491aa
Fortuna-457 <alejandro.castrocanalejo@gmail.com>
2024-03-25 17:07:04 +0100

    Cosas

--- appTemporal/urls.py
+++ appTemporal/urls.py
@@ -4,12 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Url de la página de login.
-    path('', views.loginView, name='loginView'),
-    # Url para cerrar sesión
-    path('salir/', views.logout_view, name='logout'),
-    # Url de la página principal
-    path('main/', views.index, name='main'),
-    # Url de la página de registro
-    path('singup/', views.registerView , name='singup'),
+    
 ]
