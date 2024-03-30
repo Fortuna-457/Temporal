5d959a98c9b01568249769acf2d5efeac226c9b0
Fortuna-457 <alejandro.castrocanalejo@gmail.com>
2024-03-30 11:01:45 +0100

    Login y Register

--- appTemporal/models.py
+++ appTemporal/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
+    answer_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-    asked = models.IntegerField(default=0)
--- appTemporal/views.py
+++ appTemporal/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, get_user_model, logout
+from django.http import HttpResponse
+from django.contrib.auth.decorators import user_passes_test, login_required
 
 # Create your views here.
 def index(request):
     return render(request, 'layouts/index.html')
 
+@login_required
 def games(request):
     return render(request, 'layouts/games.html')
 
