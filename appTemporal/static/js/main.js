2e8aff2af9eb26fd96d50113d5bf6f568d65a904
sofia <sofia@mulhacensoft.com>
2024-04-18 20:34:04 +0200

    sidebar in right side and hovereable

--- appTemporal/static/js/main.js
+++ appTemporal/static/js/main.js
@@ -46,64 +46,62 @@ $(document).ready(function() {
 });
 
 
-const shrink_btn = document.querySelector(".shrink-btn");
-const search = document.querySelector(".search");
-const sidebar_links = document.querySelectorAll(".sidebar-links a");
-const active_tab = document.querySelector(".active-tab");
-const shortcuts = document.querySelector(".sidebar-links h4");
-const tooltip_elements = document.querySelectorAll(".tooltip-element");
-
 let activeIndex;
 
-shrink_btn.addEventListener("click", () => {
-  document.body.classList.toggle("shrink");
+$(".shrink-btn").click(function() {
+  $("body").toggleClass("shrink");
   setTimeout(moveActiveTab, 400);
 
-  shrink_btn.classList.add("hovered");
+  $(this).addClass("hovered");
 
   setTimeout(() => {
-    shrink_btn.classList.remove("hovered");
+    $(this).removeClass("hovered");
   }, 500);
 });
 
-search.addEventListener("click", () => {
-  document.body.classList.remove("shrink");
-  search.lastElementChild.focus();
+$(".search").click(function() {
+  $("body").removeClass("shrink");
+  $(this).children().last().focus();
 });
 
 function moveActiveTab() {
   let topPosition = activeIndex * 58 + 2.5;
 
   if (activeIndex > 3) {
-    topPosition += shortcuts.clientHeight;
+    topPosition += $(".sidebar-links h4").height();
   }
 
-  active_tab.style.top = `${topPosition}px`;
+  $(".active-tab").css("top", `${topPosition}px`);
 }
 
 function changeLink() {
-  sidebar_links.forEach((sideLink) => sideLink.classList.remove("active"));
-  this.classList.add("active");
+  $(".sidebar-links a").removeClass("active");
+  $(this).addClass("active");
 
-  activeIndex = this.dataset.active;
+  activeIndex = $(this).data("active");
 
   moveActiveTab();
 }
 
-sidebar_links.forEach((link) => link.addEventListener("click", changeLink));
+$(".sidebar-links a").click(changeLink);
 
 function showTooltip() {
-  let tooltip = this.parentNode.lastElementChild;
-  let spans = tooltip.children;
-  let tooltipIndex = this.dataset.tooltip;
+  let tooltip = $(this).parent().children().last();
+  let spans = tooltip.children();
+  let tooltipIndex = $(this).data("tooltip");
 
-  Array.from(spans).forEach((sp) => sp.classList.remove("show"));
-  spans[tooltipIndex].classList.add("show");
+  spans.removeClass("show");
+  $(spans[tooltipIndex]).addClass("show");
 
-  tooltip.style.top = `${(100 / (spans.length * 2)) * (tooltipIndex * 2 + 1)}%`;
+  tooltip.css("top", `${(100 / (spans.length * 2)) * (tooltipIndex * 2 + 1)}%`);
 }
 
-tooltip_elements.forEach((elem) => {
-  elem.addEventListener("mouseover", showTooltip);
+$(".tooltip-element").mouseover(showTooltip);
+
+$(".navbar").mouseenter(function() {
+  $("body").removeClass("shrink");
 });
 
+$(".navbar").mouseleave(function() {
+  $("body").addClass("shrink");
+});
