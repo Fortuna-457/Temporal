dae6a4e7d62e05ab15597debfd8fef80d6ea57cf
Fortuna-457 <alejandro.castrocanalejo@gmail.com>
2024-06-01 13:49:25 +0200

    Merge con sofiaBranch

--- appTemporal/admin.py
+++ appTemporal/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ExtraFields, Place
+from .models import ExtraFields, Place, Question, Answer
 
 # Register your models here.
 @admin.register(ExtraFields)
@@ -9,3 +9,11 @@ class ExtraFieldsAdmin(admin.ModelAdmin):
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'type', 'answer_text', 'pub_date']
+    
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'place_id', 'difficulty', 'text', 'pub_date']
+    
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question_id', 'is_correct', 'text', 'pub_date']
